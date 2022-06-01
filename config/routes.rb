629a756209fa99df948cb64c4eb7932a43a2adba
kudou98 <fanh210mon@gmail.com>
2022-06-01 11:46:42 +0000

    [Add]Adminモデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :admins
+  devise_for :users,skip: [:passwords], controllers: {
+   registrations: "user/registrations",
+   sessions: 'user/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
