d351e94dddb954ef62b9216c293fc6745a36d55a
kudou98 <fanh210mon@gmail.com>
2022-06-01 12:56:55 +0000

    [Add]Topページ作成

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -24,4 +24,16 @@ class User::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+
+
+  def configure_sign_in_params
+    @user = User.find_by(email: params[:user][:email])
+    if @user
+      if @user.valid_password?(params[:user][:password]) && !@user.is_active
+        flash[:danger] = 'お客様は退会済みです。申し訳ございませんが、別のメールアドレスをお使いください。'
+        redirect_to new_customer_session_path
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-
-  devise_for :users,skip: [:passwords], controllers: {
-   registrations: "user/registrations",
-   sessions: 'user/sessions'
-  }
   
   
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
-}
+  }
+  
+
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+  }
+  
+  scope module: :user do
+    root 'homes#top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
