7d6cbe5c39658139e2ab65eae781b6b1c404ab01
kudou98 <fanh210mon@gmail.com>
2022-06-05 06:59:22 +0000

    [Add]コメント削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
     resources :posts, only: [:new, :create, :show, :index, :edit, :destroy]
     resources :users, only: [:show, :edit, :update]
-    resources :post_comments, only: [:create]
+    resources :post_comments, only: [:create, :destroy]
 
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
